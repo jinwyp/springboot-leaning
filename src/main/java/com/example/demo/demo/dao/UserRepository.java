0ef65ab7f3baece23f962da02b85920ca6fac63c
JinWYP <jinwyp@gmail.com>
2017-12-11 11:21:10 +0800

    update jpa and data rest

---
+++ src/main/java/com/example/demo/demo/dao/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.demo.dao;
+
+import com.example.demo.demo.entity.WebsiteUser;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<WebsiteUser, Long> {
+
+    List<WebsiteUser> findByName(@Param("name") String name);
+
+}
